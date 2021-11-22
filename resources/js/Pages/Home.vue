cdaf3e986c3478ddc6e27f8d86aaa3b3c974e724
Sani Kamal <sanikamal223@gmail.com>
2021-11-22 17:02:59 +0530

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<!--    <h1>Hello, {{name}}</h1>-->
+<!--    <p>In this starter project, We use the following frameworks:</p>-->
+<!--    <ul>-->
+<!--        <li v-for="framework of frameworks" v-text="framework"></li>-->
+<!--    </ul>-->
+    <h1>Home</h1>
+</template>
+
+<script>
+export default {
+    name: "Welcome",
+    props: {
+        name: String,
+        frameworks:Array,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
